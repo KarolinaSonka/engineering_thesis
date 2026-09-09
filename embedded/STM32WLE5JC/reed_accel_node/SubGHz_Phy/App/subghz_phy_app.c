2e782eefc7429863297270fe897361b24675c202
Karolina Sonka <karolina.sonka@outlook.com>
2026-09-09 21:58:01 +0200

    Add reed+accelerometer STM32 node

---
+++ embedded/STM32WLE5JC/reed_accel_node/SubGHz_Phy/App/subghz_phy_app.c
@@ -0,0 +1,211 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    subghz_phy_app.c
+  * @brief   Aplikacja SubGHz_Phy do cyklicznego wysyłania (LoRa P2P)
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "platform.h"
+#include "sys_app.h"
+#include "subghz_phy_app.h"
+#include "radio.h"
+
+/* USER CODE BEGIN Includes */
+#include <string.h>
+#include "utilities_def.h"
+#include "stm32_lpm.h"
+#include "usart.h"
+#include "stm32_seq.h"
+#include "stm32_timer.h"
+#include <stdio.h>
+#include "main.h"
+/* USER CODE END Includes */
+
+/* External variables ---------------------------------------------------------*/
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+#define TX_OUTPUT_POWER                             14        /* dBm */
+#define LORA_BANDWIDTH                              0         /* [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved] */
+#define LORA_SPREADING_FACTOR                       7         /* [SF7..SF12] */
+#define LORA_CODINGRATE                             1         /* [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8] */
+#define LORA_PREAMBLE_LENGTH                        8         /* Same for Tx and Rx */
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+#define TASK_TX 									(1 << CFG_SEQ_Task_TX)
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* Radio events function pointer */
+static RadioEvents_t RadioEvents;
+
+/* USER CODE BEGIN PV */
+static UTIL_TIMER_Object_t txTimer;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/*!
+ * @brief Function to be executed on Radio Tx Done event
+ */
+static void OnTxDone(void);
+
+/**
+  * @brief Function to be executed on Radio Rx Done event
+  * @param  payload ptr of buffer received
+  * @param  size buffer size
+  * @param  rssi
+  * @param  LoraSnr_FskCfo
+  */
+static void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t LoraSnr_FskCfo);
+
+/**
+  * @brief Function executed on Radio Tx Timeout event
+  */
+static void OnTxTimeout(void);
+
+/**
+  * @brief Function executed on Radio Rx Timeout event
+  */
+static void OnRxTimeout(void);
+
+/**
+  * @brief Function executed on Radio Rx Error event
+  */
+static void OnRxError(void);
+
+/* USER CODE BEGIN PFP */
+static void TxTimerCallback(void *context);
+static void TxProcess(void);
+extern void Read_ADXL345(int16_t *x, int16_t *y, int16_t *z);
+/* USER CODE END PFP */
+
+/* Exported functions ---------------------------------------------------------*/
+void SubghzApp_Init(void)
+{
+  /* USER CODE BEGIN SubghzApp_Init_1 */
+
+  /* USER CODE END SubghzApp_Init_1 */
+
+  /* Radio initialization */
+  RadioEvents.TxDone = OnTxDone;
+  RadioEvents.RxDone = OnRxDone;
+  RadioEvents.TxTimeout = OnTxTimeout;
+  RadioEvents.RxTimeout = OnRxTimeout;
+  RadioEvents.RxError = OnRxError;
+
+  Radio.Init(&RadioEvents);
+
+  /* USER CODE BEGIN SubghzApp_Init_2 */
+
+
+  Radio.SetChannel(RF_FREQUENCY);
+  Radio.SetTxConfig(
+      MODEM_LORA,
+      TX_OUTPUT_POWER,
+      0,
+      LORA_BANDWIDTH,
+      LORA_SPREADING_FACTOR,
+      LORA_CODINGRATE,
+      LORA_PREAMBLE_LENGTH,
+      LORA_FIX_LENGTH_PAYLOAD_ON,
+      true, // CRC on
+      0,    // FreqHopOn
+      0,    // HopPeriod
+      LORA_IQ_INVERSION_ON,
+      3000  // TX timeout
+  );
+
+
+  Radio.SetPublicNetwork(false);
+
+  UTIL_SEQ_RegTask(TASK_TX, 0, TxProcess);
+
+  UTIL_TIMER_Create(&txTimer,
+                    3000,
+                    UTIL_TIMER_ONESHOT,
+                    TxTimerCallback,
+                    NULL);
+
+
+  UTIL_TIMER_Start(&txTimer);
+  /* USER CODE END SubghzApp_Init_2 */
+}
+
+/* USER CODE BEGIN EF */
+
+/* USER CODE END EF */
+
+/* Private functions ---------------------------------------------------------*/
+static void OnTxDone(void)
+{
+  /* USER CODE BEGIN OnTxDone */
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+
+	UTIL_TIMER_Start(&txTimer);
+  /* USER CODE END OnTxDone */
+}
+
+static void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t LoraSnr_FskCfo)
+{
+  /* USER CODE BEGIN OnRxDone */
+  /* USER CODE END OnRxDone */
+}
+
+static void OnTxTimeout(void)
+{
+  /* USER CODE BEGIN OnTxTimeout */
+  /* USER CODE END OnTxTimeout */
+}
+
+static void OnRxTimeout(void)
+{
+  /* USER CODE BEGIN OnRxTimeout */
+  /* USER CODE END OnRxTimeout */
+}
+
+static void OnRxError(void)
+{
+  /* USER CODE BEGIN OnRxError */
+  /* USER CODE END OnRxError */
+}
+
+/* USER CODE BEGIN PrFD */
+void Master_Radio_Send(void)
+{
+	int16_t x, y, z;
+	Read_ADXL345(&x, &y, &z);
+	uint8_t reed_state = HAL_GPIO_ReadPin(REED_GPIO_Port, REED_Pin);
+	uint8_t my_tx_buffer[64];
+	uint16_t payload_len = snprintf((char*)my_tx_buffer, sizeof(my_tx_buffer), "X:%d Y:%d Z:%d D:%d", x, y, z, reed_state);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+	Radio.Send(my_tx_buffer, payload_len);
+}
+
+static void TxTimerCallback(void *context)
+{
+    UTIL_SEQ_SetTask(TASK_TX, CFG_SEQ_Prio_0);
+}
+
+static void TxProcess(void)
+{
+    Master_Radio_Send();
+}
+/* USER CODE END PrFD */
